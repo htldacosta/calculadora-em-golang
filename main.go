9928250d040311966862126f8d81b83c0c51c4fb
htldacosta <hitaludcosta@gmail.com>
2025-10-05 12:40:11 -0400

    operacao somar foi criada

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 func main(){
 	
-	
+	fmt.Println("Enter 2 numbers to sum:")
+	var num1, num2 int
+	fmt.Scan(&num1, &num2)
+
+	sum := num1 + num2
+
+	fmt.Printf("%d + %d = %d\n", num1, num2, sum)
 	
 }
