9f5c108ce8667186d0eb9c3a80af7b570ab4fb7c
htldacosta <hitaludcosta@gmail.com>
2025-10-05 13:05:43 -0400

    projeto terminado

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -12,16 +13,27 @@ func main(){
 	fmt.Scan(&input)
 
 	operation := strings.Split(input, "")
-	fmt.Println(operation[0])
-	fmt.Println(operation[1])
-	fmt.Println(operation[2])
+	
+	result := getResult(operation)
+	fmt.Printf("%s %s %s = %d\n", operation[0], operation[1], operation[2], result)
 	
 }
 
-func getResult(operation []String) int {
+func getResult(operation []string) int {
+
+	num1, _ := strconv.Atoi(operation[0])
+	num2, _ := strconv.Atoi(operation[2])
 
 	switch operation[1] {
 	case "+":
-		return operation[0] + operation[2]
+		return num1 + num2
+     case "-":
+		return num1 - num2
+	case "*":
+		return num1 * num2
+	case "/":
+		return num1 / num2	
+	default:
+		panic("Operation not valid")	
 	}
 }
