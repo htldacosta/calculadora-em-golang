9337a8a94e5d999ed4495eaf9d44834159335ed2
htldacosta <hitaludcosta@gmail.com>
2025-10-05 12:52:56 -0400

    logica de entrada de dados foi feita

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
 	
-	fmt.Println("Enter 2 numbers to sum:")
-	var num1, num2 int
-	fmt.Scan(&num1, &num2)
+	fmt.Println("Enter the operation (using this format: 2*2):")
+	var input string
+	fmt.Scan(&input)
 
-	sum := num1 + num2
-
-	fmt.Printf("%d + %d = %d\n", num1, num2, sum)
+	operation := strings.Split(input, "")
+	fmt.Println(operation[0])
+	fmt.Println(operation[1])
+	fmt.Println(operation[2])
 	
+}
+
+func getResult(operation []String) int {
+
+	switch operation[1] {
+	case "+":
+		return operation[0] + operation[2]
+	}
 }
